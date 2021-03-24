b04945344bd5ad74afbf8ddf0293823a01104178
taylahgalea <tg989@uowmail.edu.au>
2021-03-25 00:28:39 +1100

    Initial Commit

---
+++ COVID-APP/COVID_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVID_APPApp.swift
+//  COVID-APP
+//
+//  Created by Taylah Galea on 25/3/21.
+//
+
+import SwiftUI
+
+@main
+struct COVID_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
